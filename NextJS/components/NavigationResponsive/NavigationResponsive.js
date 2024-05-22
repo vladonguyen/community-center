cfd849138317adb43571246bf350125e11af79c8
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-05-23 02:08:04 +0300

    Fixed bug of nav a links not hiding in responsive

--- NextJS/components/NavigationResponsive/NavigationResponsive.js
+++ NextJS/components/NavigationResponsive/NavigationResponsive.js
@@ -5,7 +5,7 @@ export const NavigationResponsive = () =>{
 
   return  <a
   href="javascript:void(0);"
-  style={{ fontSize: 15 }}
+  style={{ fontSize: 20 }}
   className="icon"
   onClick={()=> {
     let x = document.getElementById("myTopnav");
