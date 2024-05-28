4cc4b0a0367c72bfa85c0aaf105d9df2ba7789d4
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-05-28 20:40:27 +0300

    Active link navigation fix

--- NextJS/components/MainMenu/MainMenu.js
+++ NextJS/components/MainMenu/MainMenu.js
@@ -6,72 +6,71 @@ import { NavigationResponsive } from 'components/NavigationResponsive';
 import { FaCaretDown } from 'react-icons/fa';
 import './mainmenu.css';
 
-
-// TODO responsive menu bug fixes
+// TODO responsive menu
 export const MainMenu = ({ items, callToActionLabel, callToActionDestination }) => {
-    console.log("MAIN MENU: ", items);
+    const pathname = usePathname();
 
-  const pathname = usePathname();
-  
+    console.log("Current pathname: ", pathname);
+
+    const normalizePath = (path) => {
+        if (!path) return "/";
+        return path.replace(/\/$/, '');
+    };
 
     return (
-
-        
         <nav className="topnav" id="myTopnav">
-              <div>
-                {(items || []).map((item) => (
-                   
-                    <div
-                        key={item.id}
-                    >
-                     
-                     {!item.subMenuItems?.length && (<div>
-                            <Link  href={item.destination}
-                            className={`link ${pathname === item.destination ? 'active' : ''}`}>
-                                    {item.label}
-                            </Link>
-                        </div>)}
-                        <div className="dropdown">
-                        {!!item.subMenuItems?.length && (<div>
-                         
-                                   
-                                   
-                                    <Link  href={item.destination}> {item.label} <FaCaretDown className='fa' /></Link>  
-            
- 
-                          
-
-                            
-                        </div>
+            <div>
+                {(items || []).map((item) => {
+                    const isActive = normalizePath(pathname) === normalizePath(item.destination);
+                    // console.log(`Checking path: ${normalizePath(pathname)} against item: ${normalizePath(item.destination)} - Active: ${isActive}`);
                     
-                    
-                    
-                    
-                    )}
-                          
-                       
-                     
-                        {!!item.subMenuItems?.length && (
-                           <div className="dropdown-content">
-                                {item.subMenuItems.map((subMenuItem) => (
-                                    <Link  key={subMenuItem.id} href={subMenuItem.destination} >
-                                            {subMenuItem.label} 
+                    return (
+                        <div key={item.id}>
+                            {!item.subMenuItems?.length && (
+                                <div>
+                                    <Link
+                                        className={isActive ? 'active' : ''}
+                                        href={item.destination}
+                                    >
+                                        {item.label}
                                     </Link>
-                                ))}
+                                </div>
+                            )}
+                            <div className="dropdown">
+                                {!!item.subMenuItems?.length && (
+                                    <div>
+                                        <Link
+                                            className={isActive ? 'active' : ''}
+                                            href={item.destination}
+                                        >
+                                            {item.label} <FaCaretDown className='fa' />
+                                        </Link>
+                                    </div>
+                                )}
+                                {!!item.subMenuItems?.length && (
+                                    <div className="dropdown-content">
+                                        {item.subMenuItems.map((subMenuItem) => {
+                                            const isSubActive = normalizePath(pathname) === normalizePath(subMenuItem.destination);
+                                            console.log(`Checking path: ${normalizePath(pathname)} against sub-item: ${normalizePath(subMenuItem.destination)} - Active: ${isSubActive}`);
+                                            
+                                            return (
+                                                <Link
+                                                    key={subMenuItem.id}
+                                                    className={isSubActive ? 'active' : ''}
+                                                    href={subMenuItem.destination}
+                                                >
+                                                    {subMenuItem.label}
+                                                </Link>
+                                            );
+                                        })}
+                                    </div>
+                                )}
                             </div>
-                        )}
-                         </div>
-                    </div>
-                ))}
-
-
-
-
+                        </div>
+                    );
+                })}
             </div>
-           <NavigationResponsive />
-
-    
-      </nav>
-   
+            <NavigationResponsive />
+        </nav>
     );
 };
