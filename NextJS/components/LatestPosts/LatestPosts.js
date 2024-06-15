a9cf9fd7e0d1ce5690052b1b7526de2fb92f9745
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-06-16 01:48:09 +0300

    Latest post component update

--- NextJS/components/LatestPosts/LatestPosts.js
+++ NextJS/components/LatestPosts/LatestPosts.js
@@ -2,71 +2,33 @@ import { extractListItems } from "utils/extractListItems";
 import Link from 'next/link';
 import "./latestposts.css";
 
-export const LatestPosts = ({dynamicContent}) => {
-
-
-   
-    
+export const LatestPosts = ({ dynamicContent }) => {
     const listItems = extractListItems(dynamicContent);
 
-    console.log(listItems);
+    console.log("THESE ARE LISTITEM", listItems);
+    
     return (
         <section className="news">
-  {/* TODO Needs betterment and css styling*/}
-  <h2>НОВИНИ И СЪБИТИЯ</h2>
-  <article className="news-events">
-    <div className="two-columns">
-      <div className="thumb-image">
-        <img src={listItems[0].image} alt="" />
-      </div>
-      <div className="news-content">
-        <h3>
-        {listItems[0].title}
-        </h3>
-        <p>
-        {listItems[0].content}
-        </p>
-        <p className="learn-more-link"><Link href={listItems[0].href}>Научете повече &gt;&gt;</Link></p>
-      </div>
-    </div>
-  </article>
+            {/* TODO Needs betterment and css styling*/}
+            <h2>НОВИНИ И СЪБИТИЯ</h2>
 
-  <article className="news-events">
-    <div className="two-columns">
-      <div className="thumb-image">
-        <img src={listItems[1].image} alt="" />
-      </div>
-      <div className="news-content">
-        <h3>
-        {listItems[1].title}
-        </h3>
-        <p>
-        {listItems[1].content}
-        </p>
-        <p className="learn-more-link"><Link href={listItems[1].href}>Научете повече &gt;&gt;</Link></p>
-      </div>
-    </div>
-  </article>
-
-  <article className="news-events">
-    <div className="two-columns">
-      <div className="thumb-image">
-        <img src={listItems[2].image} alt="" />
-      </div>
-      <div className="news-content">
-        <h3>
-        {listItems[2].title}
-        </h3>
-        <p>
-        {listItems[2].content}
-        </p>
-        <p className="learn-more-link"><Link href={listItems[2].href}>Научете повече &gt;&gt;</Link></p>
-      </div>
-    </div>
-  </article>
-
-</section>
-
-        
+            {listItems.map((item, index) => (
+                <article key={index} className="news-events">
+                    <div className="two-columns">
+                        <div className="thumb-image">
+                            <img src={item.image} alt="" />
+                        </div>
+                        <div className="news-content">
+                            <h3 dangerouslySetInnerHTML={{ __html: item.title }}></h3>
+                            <p> {item.date.date}.{item.date.month}.{item.date.year}</p>
+                            <p dangerouslySetInnerHTML={{ __html: item.content }}></p>
+                            <p className="learn-more-link">
+                                <Link href={item.href}>Научете повече &gt;&gt;</Link>
+                            </p>
+                        </div>
+                    </div>
+                </article>
+            ))}
+        </section>
     )
 }
--- NextJS/utils/extractListItems.js
+++ NextJS/utils/extractListItems.js
@@ -1,26 +1,31 @@
 export const extractListItems = (htmlString) => {
- // Regular expression to match <li> elements
- const liRegex = /<li\b[^>]*>(.*?)<\/li>/gs;
- const listItems = htmlString.match(liRegex) || [];
-
- 
- // Regular expressions to extract image URL, title, and content
- const imgRegex = /<img[^>]+src="([^"]+)"[^>]*>/;
- const titleRegex = /<a[^>]+href="[^"]+"[^>]*>([^<]+)<\/a>/;
- const contentRegex = /<div[^>]*class="wp-block-latest-posts__post-excerpt"[^>]*>([^<]+)<\/div>/;
- const hrefRegex = /<a[^>]+href="https:\/\/chitalishte\.ecovege\.org\/([^"]+)"[^>]*>/;
-
- return listItems.map(li => {
-     const imageMatch = li.match(imgRegex);
-     const titleMatch = li.match(titleRegex);
-     const contentMatch = li.match(contentRegex);
-     const hrefMatch = li.match(hrefRegex)
-     
-     return {
-         image: imageMatch ? imageMatch[1] : '',
-         title: titleMatch ? titleMatch[1] : '',
-         content: contentMatch ? contentMatch[1] : '',
-         href: hrefMatch ? hrefMatch[1] : '', 
-     };
- });
-}
+    // Regular expression to match <li> elements
+    const liRegex = /<li\b[^>]*>(.*?)<\/li>/gs;
+    const listItems = htmlString.match(liRegex) || [];
+  
+    // Regular expressions to extract image URL, title, content, href, and date
+    const imgRegex = /<img[^>]+src="([^"]+)"[^>]*>/;
+    const titleRegex = /<a[^>]+href="[^"]+"[^>]*>([^<]+)<\/a>/;
+    const contentRegex = /<div[^>]*class="wp-block-latest-posts__post-excerpt"[^>]*>([^<]+)/;
+    const hrefRegex = /<a[^>]+href="https:\/\/chitalishte\.ecovege\.org\/([^"]+)"[^>]*>/;
+    const dateRegex = /datetime="(\d{4})-(\d{2})-(\d{2})T/;
+  
+    return listItems.map(li => {
+      const imageMatch = li.match(imgRegex);
+      const titleMatch = li.match(titleRegex);
+      const contentMatch = li.match(contentRegex);
+      const hrefMatch = li.match(hrefRegex);
+      const dateMatch = li.match(dateRegex);
+  
+      const dateObject = dateMatch ? { year: dateMatch[1], month: dateMatch[2], date: dateMatch[3] } : null;
+  
+      return {
+        image: imageMatch ? imageMatch[1] : '',
+        title: titleMatch ? titleMatch[1] : '',
+        content: contentMatch ? contentMatch[1] : '',
+        href: hrefMatch ? hrefMatch[1] : '',
+        date: dateObject
+      };
+    });
+  }
+  
