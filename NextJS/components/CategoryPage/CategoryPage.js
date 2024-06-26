c9a95da7da3f0e990a5e85e3efef8f034492a126
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-06-26 19:31:07 +0300

    Category Posts Page optimization (refacore)

--- NextJS/components/CategoryPage/CategoryPage.js
+++ NextJS/components/CategoryPage/CategoryPage.js
@@ -11,22 +11,26 @@ export const CategoryPage = ({ categoryPosts }) => {
     console.log("categoryPosts!:", categoryPosts);
     return (
         <div>
-           {categoryPosts.eventTime==="future" && (<h1 className="cat-name">ПРЕДСТОЯЩИ {categoryPosts.name}</h1>)}
-           {categoryPosts.eventTime==="past" && (<h2 className="cat-name">МИНАЛИ {categoryPosts.name}</h2>)}
-           {categoryPosts.eventTime==="null" && (<h1 className="cat-name">{categoryPosts.name}</h1>)}
+            {categoryPosts.eventTime === "future" && (<h1 className="cat-name">ПРЕДСТОЯЩИ {categoryPosts.name}</h1>)}
+            {categoryPosts.eventTime === "past" && (<h2 className="cat-name">МИНАЛИ {categoryPosts.name}</h2>)}
+            {categoryPosts.eventTime === "null" && (<h1 className="cat-name">{categoryPosts.name}</h1>)}
             <div className="newsGalleryView">
                 {categoryPosts.map((post, index) => (
                     <div className="news-item">
                         <Link href={`/${post.node.slug}`}>
-                        
+
                             <div key={index} className="newsImgBox">
-                                <Image src={post.node.featuredImage.node.sourceUrl} alt={post.node.featuredImage.node.altText} width="250" height="250"  />
+                                <Image src={post.node.featuredImage.node.sourceUrl} alt={post.node.featuredImage.node.altText} width="250" height="250" />
                             </div>
                             <div className="text-date">
-                                <p className="cat-date">Публикувано на: <Date dateString={post.node.date} /> </p>
+                                <p className="cat-date">
+                                    {categoryPosts.catId !== 5 ? 'Публикувано на: ' : 'Дата на събитието: '}
+                                    <Date dateString={post.node.date} />
+                                </p>
                                 <h2 className="cat-title">{post.node.title}</h2>
-                                </div>
-                             </Link> </div>
+                            </div>
+
+                        </Link> </div>
                 ))}
             </div>
             {/* <div className="nextPage"
--- NextJS/utils/getCategoryPosts.js
+++ NextJS/utils/getCategoryPosts.js
@@ -1,22 +1,23 @@
-
 export const getCategoryPosts = async (catId, after = null, eventTime = null) => {
-
-  let params;
-
   const today = new Date();
   const month = today.getMonth() + 1;
   const year = today.getFullYear();
   const day = today.getDate();
 
-  // if Category is Events and dates is for future event
-  if (catId === 5 && eventTime === "future") {
+  let dateQuery = "";
+  if (catId === 5) {
+    if (eventTime === "future") {
+      dateQuery = "after: { day: $day, month: $month, year: $year }";
+    } else if (eventTime === "past") {
+      dateQuery = "before: { day: $day, month: $month, year: $year }";
+    }
+  }
 
-    params = {
-      query: `
-    query GetEvents($catId: ID!, $after: String, $day: Int!, $month: Int!, $year: Int!) {
+  const query = `
+    query GetEvents($catId: ID!, $after: String${dateQuery ? ", $day: Int!, $month: Int!, $year: Int!" : ""}) {
       category(id: $catId, idType: DATABASE_ID) {
         name
-        posts(first: 10, after: $after,  where: { dateQuery: { after: { day: $day, month: $month, year: $year }}}) {
+        posts(first: 10, after: $after, where: { dateQuery: { ${dateQuery} } }) {
           edges {
             node {
               title
@@ -26,7 +27,7 @@ export const getCategoryPosts = async (catId, after = null, eventTime = null) =>
                   altText
                 }
               }
-                date
+              date
               slug
             }
             cursor
@@ -35,111 +36,29 @@ export const getCategoryPosts = async (catId, after = null, eventTime = null) =>
         count
       }
     }
-  `, variables: {
-        catId,
-        after,
-        month,
-        year,
-        day
-      },
-    };
-    // if Category is Events and events are with past date
+  `;
 
-  } else if (catId === 5 && eventTime === "past") {
-    params = {
-      query: `
-    query GetEvents($catId: ID!, $after: String, $day: Int!, $month: Int!, $year: Int!) {
-      category(id: $catId, idType: DATABASE_ID) {
-        name
-        posts(first: 10, after: $after,  where: { dateQuery: { before: { day: $day, month: $month, year: $year }}}) {
-          edges {
-            node {
-              title
-              featuredImage {
-                node {
-                  sourceUrl
-                  altText
-                }
-              }
-                date
-              slug
-            }
-            cursor
-          }
-        }
-        count
-      }
-    }
-  `, variables: {
-        catId,
-        after,
-        month,
-        year,
-        day
-      },
-    };
-  }
-  // other category pages
-  else {
-    params = {
-      query: `
-      query GetEvents($catId: ID!, $after: String) {
-        category(id: $catId, idType: DATABASE_ID) {
-          name
-          posts(first: 10, after: $after, ) {
-            edges {
-              node {
-                title
-                featuredImage {
-                  node {
-                    sourceUrl
-                    altText
-                  }
-                }
-                  date
-                slug
-              }
-              cursor
-            }
-          }
-          count
-        }
-      }
-    `, variables: {
-        catId,
-        after
-      },
-    };
-  }
-
+  const variables = { catId, after, ...(dateQuery && { day, month, year }) };
 
   const response = await fetch(process.env.WP_GRAPHQL_URL, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(params),
+    body: JSON.stringify({ query, variables }),
   });
 
   const result = await response.json();
-  // console.log("RESULT", result)
   const posts = result.data.category.posts.edges;
 
-  // Process posts
-  posts.forEach(post => {
-    // console.log("NODE TITLE", post.node.title);
-  });
-
-  // Update the `after` variable for the next request
   if (posts.length > 0) {
     after = posts[posts.length - 1].cursor;
-    // console.log("AFTER", after);
   }
+
   posts.after = after;
   posts.name = result.data.category.name;
   posts.catId = catId;
   posts.eventTime = eventTime;
-  // console.log("POSTNODE", posts[0])
 
   return posts;
 }
