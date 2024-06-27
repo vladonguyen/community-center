6c6dbb93d3ea6704358722f247bf8057e04c6095
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-06-27 04:49:55 +0300

    Formspree component update

--- NextJS/components/FormspreeForm/FormspreeForm.js
+++ NextJS/components/FormspreeForm/FormspreeForm.js
@@ -1,28 +1,35 @@
 "use client";
+
+import "./formspreeform.css";
 import { useForm, ValidationError } from "@formspree/react";
 import { Input } from "components/Input";
 
 export const FormspreeForm = ({ formId }) => {
   const [state, handleSubmit] = useForm(formId);
   if (state.succeeded) {
-    return <p className="max-w-5xl mx-auto my-5">Thanks for joining!</p>;
+    return <p className="max-w-5xl mx-auto my-5">Благодарим Ви, Вашето съобщение беше изпратено!</p>;
   }
   return (
-    <form onSubmit={handleSubmit} className="max-w-5xl mx-auto min-w-max border border-slate-700 p-10">
-<label for="email" className="block font-bold">Email Address</label>
-      <Input id="email" type="email" name="email" />
+    <form onSubmit={handleSubmit} className="contact-form">
+<label for="name" className="label-contact">Име</label>
+      <Input id="name" type="name" name="name"  className="input-contact name" />
+      <ValidationError prefix="Name" field="name" errors={state.errors} />
+
+
+<label for="email" className="block font-bold">Email</label>
+      <Input id="email" type="email" name="email" className="input-contact email" />
       <ValidationError prefix="Email" field="email" errors={state.errors} />
       
-      <label for="message" className="block mt-5 font-bold">Message</label>
+      <label for="message" className="block mt-5 font-bold">Съобщение:</label>
       <textarea
-        className="border-2 border-slate-400 p-1 hover:border-slate-500"
+       className="textarea-contact message"
         id="message"
         name="message"
       />
       <ValidationError prefix="Message" field="message" errors={state.errors} />
       <div>
-        <button className="btn" type="submit" disabled={state.submitting}>
-          Submit
+        <button className="btn-contact" type="submit" disabled={state.submitting}>
+          Изпрати
         </button>
       </div>
     </form>
