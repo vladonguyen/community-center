003ffbbd3ebcd9e19d32cb13d9a2ac8e854a5ba9
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-06-29 05:49:01 +0300

    Added loading alt

--- NextJS/app/loading.js
+++ NextJS/app/loading.js
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Loading() {
   return (
-    <Image src="./img/loading.svg" width={50} height={50} className="loading-icon"/>
+    <Image src="./img/loading.svg" width={50} height={50} className="loading-icon" alt="loading..."/>
   );
 }
