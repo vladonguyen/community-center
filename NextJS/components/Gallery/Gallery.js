24f6c47bfc7911391e01a0dc1ca3b86a7c720103
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-07-03 03:09:28 +0300

    Update Gallery.js

--- NextJS/components/Gallery/Gallery.js
+++ NextJS/components/Gallery/Gallery.js
@@ -1,30 +1,30 @@
 import "./gallery.css";
 import LigthBox from "components/LigthBox/LigthBox";
 
-export const Gallery = ({columns, cropImages, items}) => {
+export const Gallery = ({ columns, cropImages, items }) => {
     let maxHeight = 0;
     let maxWidth = 0;
-    if(cropImages){
+    if (cropImages) {
         items.forEach(item => {
-            if(item.attributes.height > maxHeight){
+            if (item.attributes.height > maxHeight) {
                 maxHeight = item.attributes.height;
                 maxWidth = item.attributes.width;
             }
-            
+
         });
     }
 
-    const imagesSrc =items.map(item => item.attributes.url);
-    console.log("imagesSrc!!!", imagesSrc)
+    const imagesSrc = items.map(item => item.attributes.url);
+
     return <div className="flex flex-wrap max-w-5xl mx-auto">
-     
+
         {items.map((item, index) => (
-            <div 
-            key={item.id}
-            
-             className="p-5 flex-grow"
+            <div
+                key={item.id}
+
+                className="p-5 flex-grow"
             >
-                <LigthBox imagesSrc={imagesSrc} item={item} index={index}/>
+                <LigthBox imagesSrc={imagesSrc} item={item} index={index} />
             </div>
         ))}
 
